451fe19e60ccc62f4abd05992a5ae712bd82c3be
ahmad4948 <ahmadshamekh99@gmail.com>
2025-09-17 14:03:57 +0300

    Initial project setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header (){
+    return(
+        <div className="flex mt-4">
+      <h1 className="ml-32 text-2xl ">Shopping Cart</h1>
+      <nav>
+       <a className="text-2xl">Home</a>
+       <a className="text-2xl">Cart</a>
+      </nav>
+      </div>
+    )
+}
+export default Header
